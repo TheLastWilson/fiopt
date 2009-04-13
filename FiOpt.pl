3d52fa0c6380fe324c1a0253dc8cb7df77f1b184
craig <craig@LanBox.(none)>
2009-04-13 16:24:54 +0100

    setup single rule ip tables structure, ready for rule disection

--- FiOpt.pl
+++ FiOpt.pl
@@ -19,16 +19,21 @@
 #   -f     File
 #   -r     single rule
 #
-if ( $ARGV[1] ne "")
+if ($ARGV[1] ne "")
 { 
  if ($ARGV[0] eq "-i")
  { 
   if ($ARGV[1] eq "-r")
-  { &iptables_single; }
+  { 
+   if ($ARGV[2])
+   { &iptables_single($ARGV[2]); }
+   else
+   { &help; }
+  }
   elsif ($ARGV[1] eq "-f")
   { print "Linux IpTables Rule File\n"; }
   else
-  { &help }
+  { &help; }
  }
  elsif ($ARGV[0] eq "-a")
  { 
@@ -38,7 +43,7 @@ if ( $ARGV[1] ne "")
   elsif ($ARGV[1] eq "-f")
   { print "Cisco ACLs Rule File\n"; }
   else 
-  { &help }
+  { &help; }
  }
  else
  { &help; }
@@ -70,12 +75,44 @@ sub help
 
  Input Types
  -f	File
- -r	single rule
+ -r	Single Rule (followed by the rule surrounded in either " or ')
+        Single rules must be an Apple rule with -A
 
 ENDHELP
 }
 
 sub iptables_single
 {
- print "Linux Iptables Single Rule\n";
+ local (@rule, $append, $dPort, $dIP, $sIP, $sPort, $jump);
+ print "Insert Single IPtables Rule\n$_[0] \n";
+ @rule = split(' ',$_[0]);
+ 
+ $append = 0; # set append to 0. 0 = no apppend flag, 1 = append flag
+ foreach $element(@rule) 
+ {
+  if ($element eq '-A')
+  {
+   $append = 1; # 1 = append flag detected
+  }
+ }
+ 
+ # if append flag was detected (1) then disect rule for entry into database
+ if ($append = 1)
+ {
+  print "Append rule: true";
+ }
+ else
+ {
+  print "Error: single rule entry must contain Append flag (-A)";
+ }
 }
+
+
+
+
+
+
+
+
+
+
